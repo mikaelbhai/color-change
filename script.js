4c047524da057fd34b62719f0d120e707071e738
MikaelBhai <126885689+mikaelbhai@users.noreply.github.com>
2024-09-14 06:42:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const colors = [
     '#F6FF33', '#B533FF', '#5733FF', '#33FF57', '#33FFF6', '#FF33B5', '#57FF33', '#FFB533', '#33A1FF',
     '#FF33F6', '#A1FF33', '#33FF33', '#FF5733', '#8D33FF', '#33FFB5', '#F6FF33', '#B533FF', '#FF33A1',
     '#33FFA1', '#A1FF33', '#33FF8D', '#FFB533', '#F6FF33'
+    
 ];
 
 let currentIndex = 0;
